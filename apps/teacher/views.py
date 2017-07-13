a9a46be6981c4ae3a15744aaee88081b2d28d984
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-07-12 21:20:19 -0400

    Agregado el render de la lista de preguntas

--- apps/teacher/views.py
+++ apps/teacher/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 from apps.course.models import Course
 from apps.formative.models import Formative
+from apps.teacher.models import Teacher, Question
 
 
 @login_required
@@ -19,3 +21,29 @@ def index(request):
       'formatives': formatives ,
       'total_formatives': total_formatives
       })
+
+
+@login_required
+def view_questions(request):
+    user = request.user.teacher
+    questions = user.question.filter(teacherhasquestion__deleted=0)
+
+    #questions = {}
+    #cont = 0
+    #for question in query:
+    #    questions[cont] = {
+    #      'id': question.id,
+    #      'title': question.title, 
+    #      'type': question.type,
+    #      'creation_date': question.creation_date,
+    #      'share': question.share,
+    #      'code': question.code,
+    #      'url': question.url,
+    #      'extension': question.extension}
+    #    cont = cont + 1
+
+    #questions = JsonResponse(questions)
+    return render(request, 'teacher/questions.html', {'questions': questions})
+    
+    #Revisando el código de MAS para traer todas las preguntas.
+    #Deben devolverse en un JSON para que el html pueda trabajarlos.
