4ecb2f576de7ba40e1bbf5cbedea2ccbd4c31471
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-07-14 00:57:12 -0400

    Identation correction

--- apps/course/forms.py
+++ apps/course/forms.py
@@ -8,8 +8,8 @@ class CourseForm(forms.ModelForm):
         model = Course
         fields = ('name', 'code', 'semester', 'description')
         widgets = {
-        	'name': forms.TextInput(attrs={'class':'validate', 'length': 50, 'data-length': 50}),
-        	'code': forms.TextInput(attrs={'class':'validate', 'length': 20, 'data-length': 20}),
-        	'semester': forms.Select(choices=(('1', 'I Semestre',),('2', 'II Semestre',))),
-        	'description': forms.Textarea(attrs={'class':'materialize-textarea', 'length': 200, 'data-length': 200}),
+            'name': forms.TextInput(attrs={'class':'validate', 'length': 50, 'data-length': 50}),
+            'code': forms.TextInput(attrs={'class':'validate', 'length': 20, 'data-length': 20}),
+            'semester': forms.Select(choices=(('1', 'I Semestre',),('2', 'II Semestre',))),
+            'description': forms.Textarea(attrs={'class':'materialize-textarea', 'length': 200, 'data-length': 200}),
         }
