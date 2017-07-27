66924f776d5089edd131b70c9b4e8d34370d9514
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-07-26 21:55:09 -0400

    Add function preview_question

--- static/js/teacher.questions.js
+++ static/js/teacher.questions.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
   /* Share/don't share question */
   $("input[type='checkbox']").change(function(){ 
     id_checkbox = $(this)[0].id;
-    share(id_checkbox);
+    share_question(id_checkbox);
+  });
+
+  /* Preview question */
+  $("button[data-action='preview']").click(function() {
+    id_question = $(this)[0].id;
+    preview_question(id_question)
   });
 });
 
-function share(id_checkbox) {
+function share_question(id_checkbox) {
   $.ajax({
     type: 'POST',
     url: document.location.pathname + "/share/" + id_checkbox,
@@ -22,5 +28,36 @@ function share(id_checkbox) {
   })
   .fail(function() {
     Materialize.toast('Un error a ocurrido. Intente nuevamente', 5000, 'rounded');
+  });
+};
+
+function preview_question(id_question) {
+  $.ajax({
+    type: "POST",
+    url: document.location.pathname,
+    headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+    data: {
+      action: "preview",
+      id_question: id_question
+    },
+    dataType: 'json',
+  })
+  .done(function(data){
+    if (data.result == "error") {
+        Materialize.toast(data.message, 3000,'rounded');        
+    } else {
+        //Materialize.toast('Pregunta valida', 3000,'rounded');
+        url           = data.url;
+        tipo_pregunta = data.type;
+        extension     = data.extension;
+        codigo        = data.code;
+        opcion        = "visualizar";
+        xml_archivo   = $.parseXML(data.archivo);
+        xml_manifest  = $.parseXML(data.imsmanifest);
+        select_method()
+    }
+  })
+  .fail(function() {
+    Materialize.toast('Un error a ocurrido. Intente nuevamente', 3000, 'rounded');
   });
 };
