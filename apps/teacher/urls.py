94bb49220a7798edb3bff4561d9ec5047a1b0835
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-07-27 03:11:02 -0400

    Add url for download and delete questions

--- apps/teacher/urls.py
+++ apps/teacher/urls.py
@@ -4,8 +4,10 @@ from apps.teacher.views import *
 
 urlpatterns = [
     url(r'^home', index, name='home'),
-    url(r'^mis-preguntas$', view_questions , name='questions'),
-    url(r'^agregar-pregunta$', add_question , name='add_question'),
-    url(r'^mis-preguntas/share/(?P<question_id>\d+)$', share_question , name='share_question'),
+    url(r'^mis-preguntas$', view_questions, name='questions'),
+    url(r'^agregar-pregunta$', add_question, name='add_question'),
+    url(r'^mis-preguntas/share/(?P<question_id>\d+)$', share_question, name='share_question'),
+    url(r'^mis-preguntas/download/(?P<question_id>\d+)$', download_question, name='download_question'),
+    url(r'^mis-preguntas/delete/(?P<question_id>\d+)$', download_question, name='delete_question'),
     url(r'^delete-folder$', delete_folder),
 ]
