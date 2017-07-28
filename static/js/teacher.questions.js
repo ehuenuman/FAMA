d1690b228087b26b98e370c5476d7e5627f7614f
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-07-28 00:05:14 -0400

    Download question xml and zip

--- static/js/teacher.questions.js
+++ static/js/teacher.questions.js
@@ -68,6 +68,85 @@ function preview_question(id_question) {
   });
 };
 
-function download_question(id_question) {
-  window.open(document.location.pathname+"/download/"+id_question, "_blank");  
+function download_question(id_question) {  
+  /*
+  $.fileDownload(document.location.pathname+"/download/"+id_question, {
+    failCallback: function (html, url) {
+      Materialize.toast('Error al obtener la pregunta.', 3000, 'rounded');  
+    }
+  });
+  */
+  /*
+  $.fileDownload(document.location.pathname+"/download", {
+    //preparingMessageHtml: "We are preparing your report, please wait...",
+    //failMessageHtml: "There was a problem generating your report, please try again.",
+    httpMethod: "POST",
+    data: {id_question: id_question}
+  });
+  */
+  
+  $.ajax({
+    type: "POST",
+    url: document.location.pathname+"/download",
+    headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+    data: {id_question: id_question},  
+  })
+  .done(function( data, textStatus, jqXHR ) {    
+    content_type = jqXHR.getResponseHeader("content-type");
+    if ( content_type == "text/xml" ) {       
+      var file = new File([jqXHR.responseText], jqXHR.getResponseHeader("Name"), {type: content_type});
+      saveAs(file)
+    } else {
+      if ( content_type == "application/zip" ) {
+        $.fileDownload(document.location.pathname+"/download/"+id_question, {
+          failCallback: function (html, url) {
+            Materialize.toast('Error al obtener la pregunta', 3000, 'rounded');  
+          }
+        });        
+        /*
+        console.log(jqXHR)
+        console.log(jqXHR.getAllResponseHeaders())
+        console.log(data)
+        console.log(data.length);
+        var buffer = new Uint8Array(str2bytes(data)).buffer;
+        var blob = new Blob([buffer], {type: content_type});    
+        console.log(blob.size);        
+        saveAs(blob, jqXHR.getResponseHeader("Name"));
+        */
+      } else {
+        Materialize.toast(data.message, 3000, 'rounded');
+      }
+    }
+  })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+    Materialize.toast('Un error ha ocurrido. Intente nuevamente', 3000, 'rounded');
+  });
+  
+  //window.open(document.location.pathname+"/download/"+id_question, "_blank");
 };
+
+function str2bytes (str) {
+    /*
+    var bytes = new Uint8Array(str.length);
+    for (var i=0; i<str.length; i++) {
+        bytes[i] = str.charCodeAt(i);
+    }
+    */
+    var bytes = []; // char codes
+    var bytesv2 = []; // char codes
+
+    for (var i = 0; i < str.length; ++i) {
+      var code = str.charCodeAt(i);
+
+      bytes = bytes.concat([code]);
+
+      bytesv2 = bytesv2.concat([code & 0xff, code / 256 >>> 0]);
+    }
+
+    // 72, 101, 108, 108, 111, 31452
+    //console.log('bytes', bytes.join(', '));
+
+    // 72, 0, 101, 0, 108, 0, 108, 0, 111, 0, 220, 122
+    //console.log('bytesv2', bytesv2.join(', '));
+    return bytesv2;
+}
