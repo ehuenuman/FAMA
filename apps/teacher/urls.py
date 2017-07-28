5290bc3bedf4b9bf861cdfedaa9d510862e255dc
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-07-28 00:08:53 -0400

    Urls for download xml and zip files question

--- apps/teacher/urls.py
+++ apps/teacher/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^mis-preguntas$', view_questions, name='questions'),
     url(r'^agregar-pregunta$', add_question, name='add_question'),
     url(r'^mis-preguntas/share/(?P<question_id>\d+)$', share_question, name='share_question'),
-    url(r'^mis-preguntas/download/(?P<question_id>\d+)$', download_question, name='download_question'),
+    url(r'^mis-preguntas/download$', download_question),
+    url(r'^mis-preguntas/download/(?P<question_id>\d+)$', download_question),
     url(r'^mis-preguntas/delete/(?P<question_id>\d+)$', download_question, name='delete_question'),
     url(r'^delete-folder$', delete_folder),
 ]
