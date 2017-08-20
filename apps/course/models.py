70b6428c4f5a06c9b900ce3e16132411554593cd
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-08-20 03:23:46 -0300

    Update data model

--- apps/course/models.py
+++ apps/course/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    id = models.CharField(primary_key=True, max_length=100)
+    id_char = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=20)
     semester = models.CharField(max_length=10)
     year = models.CharField(max_length=4)
-    description = models.CharField(max_length=200)
-    creation_date = models.DateTimeField(blank=False)
+    description = models.CharField(max_length=200, blank=True)
+    creation_date = models.DateTimeField()
     teacher = models.ForeignKey('teacher.Teacher', models.DO_NOTHING)
     student = models.ManyToManyField('student.Student', through='CourseHasStudent')
 
