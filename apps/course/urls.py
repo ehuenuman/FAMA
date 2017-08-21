ca46e8b222d26d07ca89c7e056ee18398c07f751
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-08-21 00:38:13 -0300

    Add url for get all courses

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^nuevo', create_course, name='create'),
     url(r'^$', view_courses, name='index'),
+    url(r'^nuevo$', create_course, name='create'),
+    url(r'^all$', get_courses),
     url(r'^(?P<course_id_char>\w+)$', show_course, name='show'),
     url(r'^(?P<course_id_char>\w+)/agregar-estudiantes$', add_students, name='add_students'),
     url(r'^(?P<course_id>\d+)/download-csv$', download_csv, name='download_csv'),
     url(r'^editar/(?P<course_id_char>\w+)$', edit_course, name='edit'),
     url(r'^borrar/(?P<course_id>\d+)$', delete_course, name='delete'),
-    url(r'^all$', get_courses),
 ]
