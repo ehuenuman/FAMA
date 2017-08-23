5ffa88381e8c4893505869b9a13562fc29ccba50
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-08-22 23:50:11 -0300

    Related changes to model for Play

--- apps/play/models.py
+++ apps/play/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Play(models.Model):
+    creation_play = models.DateTimeField()
+    duration = models.DurationField()
+    start_play = models.DateTimeField()
+    limit_time = models.DurationField()
+    close_play = models.DateTimeField()
+    is_active = models.IntegerField()
+    formative = models.ForeignKey('formative.Formative', models.DO_NOTHING)
+    course = models.ForeignKey('course.Course', models.DO_NOTHING)
+    #student = models.ManyToManyField('student.Student', through='student.Answer')
+    #question = models.ManyToManyField('teacher.Question', through='student.Answer')
+
+    class Meta:
+        managed = False
+        db_table = 'play'
+        ordering = ('creation_play', 'start_play')
