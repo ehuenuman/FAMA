b7bbae0bd18940b5a8bd1377c769d3f76d90a6b8
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-08-24 19:28:21 -0300

    Delete redundatn fiels of Studet Model

--- apps/student/models.py
+++ apps/student/models.py
@@ -1,24 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 #from teacher.models import Formative
 
 # Create your models here.
-class Student(models.Model):    
-    rut = models.CharField(max_length=15)
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, blank=True)
+class Student(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    rut = models.CharField(unique=True, max_length=15)
 
     class Meta:
         managed = False
-        db_table = 'student'
-        unique_together = (('id', 'rut'),)
+        db_table = 'student'        
 
 
 class Answer(models.Model):    
     answer = models.CharField(max_length=50)
     correct = models.IntegerField()
-    date = models.DateTimeField(blank=True, null=True)
+    date = models.DateTimeField()
     student = models.ForeignKey('Student', models.DO_NOTHING)
     play = models.ForeignKey('play.Play', models.DO_NOTHING)
     question = models.ForeignKey('teacher.Question', models.DO_NOTHING)
