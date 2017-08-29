0dfcf0cbaf4b5e9c89491696d60e8fa892c2e11c
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-08-29 15:44:26 -0300

    Return question data to show question to student

--- apps/play/views.py
+++ apps/play/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.utils import timezone
 from django.core import serializers
 from datetime import timedelta
+import xml.etree.ElementTree as ET
 
 from apps.question.views import question_data
 from .models import Play
@@ -39,15 +40,36 @@ def show_play(request, play_id_char):
 
 @login_required
 def reply_play(request, play_id_char, question_id):
-    print(play_id_char, question_id)
+    #print(play_id_char, question_id)
     play = Play.objects.get(id_char=play_id_char)
     formative = Formative.objects.get(id=play.formative.id)
     if validate_question(formative, question_id):
+        namespace = {"ns1": "http://www.imsglobal.org/xsd/imsqti_v2p1",
+            "ns2": "http://www.w3.org/2001/XMLSchema-instance",
+            "ns3": "http://www.imsglobal.org/xsd/imsqti_v2p1  http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1.xsd"}
         question = Question.objects.get(id=question_id)
         data = question_data(question)
         if question.type == "choice":
-            return render(request, "question/reply_choice.html", {
-                "question": data
+            print(data["archivo"])
+            root = ET.fromstring(data["archivo"])
+            print(root)
+            data = {}
+            alternatives = []
+            data["title"] = root.get("title")
+            data["question"] = root.find("*//ns1:prompt", namespace).text
+            for elem in root.iterfind("*//ns1:simpleChoice", namespace):
+                alternatives.append({"text": elem.text, "id": elem.get("identifier")})
+                print(elem.get("identifier"), elem.text)
+
+            data["alternatives"] = alternatives
+
+            print(data)            
+
+            return render(request, 
+                "question/reply_choice.html", 
+                {
+                    "question": data,
+                    "alternatives": alternatives
                 })
     else:
         return redirect('play:show', play_id_char=play_id_char)
