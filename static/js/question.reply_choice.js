d044cb1f378c26c01126910a55160bf430054d96
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-08-31 22:59:06 -0300

    Add countdown script to close_reply

--- static/js/question.reply_choice.js
+++ static/js/question.reply_choice.js
@@ -3,6 +3,23 @@ $(document).ready(function() {
     $(this).children("input")[0].click();
   });
 
+  $('#timer').countdown($("#timer").data("limit-time"))
+  .on('update.countdown', function(event) {
+    var format = '%H:%M:%S';
+    if(event.offset.totalDays > 0) {
+      format = '%-d day%!d ' + format;
+    }
+    if(event.offset.weeks > 0) {
+      format = '%-w week%!w ' + format;
+    }
+    $(this).html(event.strftime(format));
+  })
+  .on('finish.countdown', function(event) {
+    $(this).html('Tiempo Completado')
+      .parent().addClass('disabled');
+
+  });
+
   $("a.reply-btn").click(function(event) {
     if ($("li.collection-item input:checked").length == 1) {
       console.log($("a.reply-btn").data("question"));
