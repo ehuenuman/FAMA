1543af003a586b3e7f93e175e606354e1cd89eaf
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-02 02:46:04 -0300

    Send POST request when the student responds

--- static/js/question.reply_choice.js
+++ static/js/question.reply_choice.js
@@ -5,8 +5,23 @@ $(document).ready(function() {
 
   $("a.reply-btn").click(function(event) {
     if ($("li.collection-item input:checked").length == 1) {
-      console.log($("a.reply-btn").data("question"));
-      console.log($("a.reply-btn").data("play"));
+      $.ajax({
+        url: document.location.pathname,
+        type: 'POST',
+        headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+        dataType: "JSON",
+        data: {answer: $("li.collection-item input:checked")[0].id},
+      })
+      .done(function(data) {
+        if (data.data == "OK") {
+          Materialize.toast("Respuesta guardada", 1000, "rounded")
+        } else {
+          Materialize.toast(data.data, 3000, "rounded")
+        }
+      })
+      .fail(function() {
+        Materialize.toast("Un error ha ocurrido", 3000, "rounded")
+      });
     } else {
        Materialize.toast("Selecciona una alternativa", 3000, "rounded")
     }
