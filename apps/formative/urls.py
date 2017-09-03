3f2cf74fc347d1a1a468aa3ce929810b69c214f9
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-03 15:25:19 -0300

    Add / to final urls

--- apps/formative/urls.py
+++ apps/formative/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', view_formatives, name='index'),
-    url(r'^nuevo$', create_formative, name='create'),
-    url(r'^play$', start_formative),    
+    url(r'^nuevo/$', create_formative, name='create'),
+    url(r'^play/$', start_formative),    
     url(r'^editar/(?P<formative_id>\d+)$', edit_formative, name='edit'),
     url(r'^(?P<formative_id>\d+)$', show_formative, name='show'),
     #url(r'^borrar/(?P<formative_id>\d+)$', delete_course, name='delete'),
