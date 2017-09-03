e423a6480c161bda817f5b8c230ab815daa24b56
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-03 01:26:02 -0300

    Delete unused function

--- static/js/login.js
+++ static/js/login.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-  $("form#login_alumno input[name='username']").keyup(function(event) {
-    var input = $("form#login_alumno input[name='username']");    
+  $("ul.tabs").tabs();
+  $("form#student_log input[name='username']").keyup(function(event) {
+    var input = $("form#student_log input[name='username']");    
     var rut = input.val().replace("-", "");
     if (rut.length > 1  ) {
       var sub1 = rut.substring(0, rut.length - 1);
@@ -10,26 +11,5 @@ $(document).ready(function() {
       var rut = input.val().replace("-", "");
       input.val(rut);
     }
-  });
+  });  
 });
-
-function switchForm() {
-  if ($("input[type='checkbox']")[0].checked) {
-    logTeacher();
-  }
-  else {
-    logStudent();
-  }
-}
-
-function logTeacher() {
-  $("input[type='checkbox']")[0].checked = true;
-  $("#login_alumno").hide(1000); 
-  $("#login_profesor").show(1000);
-}
-
-function logStudent() {
-  $("input[type='checkbox']")[0].checked = false;
-  $("#login_profesor").hide(1000);
-  $("#login_alumno").show(1000);    
-}
