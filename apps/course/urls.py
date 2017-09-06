cb8cdd65c880951d85f85cc22e7d761380baefdf
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-05 21:59:26 -0300

    Add namespace url all courses

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     url(r'^$', view_courses, name='index'),
     url(r'^nuevo/$', create_course, name='create'),
-    url(r'^all$', get_courses),
+    url(r'^all$', get_courses, name='all'), #only POST
     url(r'^(?P<course_id_char>\w+)/$', show_course, name='show'),
     url(r'^(?P<course_id_char>\w+)/agregar-estudiantes$', add_students, name='add_students'),
     url(r'^(?P<course_id>\d+)/download-csv$', download_csv, name='download_csv'),
