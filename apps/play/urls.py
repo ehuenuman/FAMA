f291bef9d7aa8c884e53bdeeb33594d573a291e6
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-05 22:01:10 -0300

    Add namespace to url stop play

--- apps/play/urls.py
+++ apps/play/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from apps.play.views import *
 
 urlpatterns = [
-    url(r'^stop$', stop_play),
+    url(r'^stop$', stop_play, name='stop'), #Only POST
     url(r'^(?P<play_id_char>\w+)/$', show_play, name='show'),
     url(r'^(?P<play_id_char>\w+)/reply/(?P<question_id>\d+)/$', reply_play, name='reply'),
     url(r'^(?P<play_id_char>\w+)/dashboard/$', play_result, name='result'),
