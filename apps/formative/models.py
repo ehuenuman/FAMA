4c5fc8d468d1f8104d53b9f5df7a41b6396de6be
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-11 11:48:11 -0300

    Now formative description is optionally

--- apps/formative/models.py
+++ apps/formative/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Formative(models.Model):
     #id = models.AutoField()
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     creation_date = models.DateTimeField()
     teacher = models.ForeignKey('teacher.Teacher', models.DO_NOTHING)
     course = models.ManyToManyField('course.Course', through='play.Play')
