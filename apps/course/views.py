5c48678e96d2b0695218298bdd2c5acda85a8805
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-09-14 10:37:16 -0300

    Now work with xls file for template students

--- apps/course/views.py
+++ apps/course/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
 from django.utils import timezone
 from datetime import date
+import xlrd
 import csv, os
 
 from play.settings import BASE_DIR
@@ -90,51 +91,57 @@ def add_students(request, course_id_char):
         if request.FILES:
             csvfile = request.FILES['csv_file']
             
-            fout = open(BASE_DIR+'/templates/course/{0}.csv'.format(course_id), 'wb')
+            fout = open(BASE_DIR+'/templates/course/{0}.xls'.format(course_id), 'wb')
             for chunk in csvfile.chunks():
                 fout.write(chunk)
             fout.close() 
 
-            with open(BASE_DIR+'/templates/course/{0}.csv'.format(course_id)) as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    #print(row['RUT'], row['NOMBRES'], row['APELLIDOS'], row['EMAIL'])  
-                    if Student.objects.filter(rut=row['RUT'].upper()):
-                        student = Student.objects.get(rut=row['RUT'].upper())
-                        ##print("Existe entre todos los estudiantes")
-                        try:
-                            CourseHasStudent.objects.get(course=course, student=student)
-                            #print("Existe en el curso")
-                        except Exception as e:
-                            #print("No existe en el curso")
-                            m = CourseHasStudent(course=course, student=student)
-                            m.save()
-                            data[student.user.id] = {'rut': student.rut, 'name': student.user.first_name,
-                                                'last_name': student.user.last_name}
-                    else:
-                        #print("Nuevo")
-                        user = User.objects.create_user(
-                            username=row['RUT'].upper(),
-                            password=row['RUT'].upper(),
-                            first_name=row['NOMBRES'],
-                            last_name=row['APELLIDOS'])
-                        user.save()
-                        student = Student.objects.create(
-                            user=user,
-                            rut=row['RUT'].upper())
-
-                        group = Group.objects.get(name='Students')
-                        user.groups.add(group)
-                        user.save()
-
+            book = xlrd.open_workbook(BASE_DIR+'/templates/course/{0}.xls'.format(course_id))
+            sh = book.sheet_by_index(0)
+            for rx in range(1, sh.nrows):
+                rx_rut = sh.row(rx)[0].value.strip(" ").lstrip("0").upper()           
+                if Student.objects.filter(rut=rx_rut):
+                    #Account exist
+                    student = Student.objects.get(rut=rx_rut)                
+                    try:
+                        #In this course
+                        CourseHasStudent.objects.get(course=course, student=student)
+                    except Exception as e:
+                        #Havent this course
                         m = CourseHasStudent(course=course, student=student)
                         m.save()
                         data[student.user.id] = {'rut': student.rut, 'name': student.user.first_name,
-                                            'last_name': student.user.last_name}
-                    student = None
-                    m = None
+                                                'last_name': student.user.last_name}
+                else:
+                    #New student                    
+                    rx_last_name = sh.row(rx)[1].value.split(',')[0].strip(" ").title()
+                    rx_first_name = sh.row(rx)[1].value.split(',')[1].strip(" ").title()
+                    rx_email = sh.row(rx)[2].value.strip(" ")
+                    rx_study = sh.row(rx)[3].value.strip(" ").title()
+                    
+                    user = User.objects.create_user(
+                        username=rx_rut,
+                        password=rx_rut,
+                        first_name=rx_first_name,
+                        last_name=rx_last_name)
+                    user.save()
+                    student = Student.objects.create(
+                        user=user,
+                        rut=rx_rut,
+                        study=rx_study)
 
-            os.remove(BASE_DIR+'/templates/course/{0}.csv'.format(course_id))
+                    group = Group.objects.get(name='Students')
+                    user.groups.add(group)
+                    user.save()
+
+                    m = CourseHasStudent(course=course, student=student)
+                    m.save()
+                    data[student.user.id] = {'rut': student.rut, 'name': student.user.first_name,
+                                        'last_name': student.user.last_name}
+                student = None
+                m = None
+            
+            os.remove(BASE_DIR+'/templates/course/{0}.xls'.format(course_id))            
         else:            
             for i in request.POST.lists():
                 if Student.objects.filter(rut=i[1][0].upper()):
@@ -176,16 +183,24 @@ def add_students(request, course_id_char):
 
 
 @login_required
-def download_csv(request, course_id):
-    course = Course.objects.get(id=course_id)
+def download_template(request, course_id):
+    if request.method == "GET":
+        course = Course.objects.get(id=course_id)
 
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="EstudiantesCurso-{0}.csv"'.format(course.name)
+        #response = HttpResponse(content_type='text/csv')
+        #response['Content-Disposition'] = 'attachment; filename="EstudiantesCurso-{0}.csv"'.format(course.code)
 
-    writer = csv.writer(response)
-    writer.writerow(['RUT', 'NOMBRES', 'APELLIDOS', 'EMAIL'])    
+        #writer = csv.writer(response)
+        #writer.writerow(['RUT', 'NOMBRES', 'APELLIDOS', 'EMAIL'])
 
-    return response
+        xls = open(BASE_DIR+"/templates/course/StudentsTemplate.xls", "rb")
+
+        response = HttpResponse(xls, content_type="application/vnd.ms-excel")
+        response["content-Disposition"] = "attachment; filename='EstudiantesCurso-{0}.xls'".format(course.code)
+
+        xls.close()
+
+        return response
 
 @login_required
 def get_courses(request):    
