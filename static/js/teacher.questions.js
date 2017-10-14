dfd986cb6375222e287887cc06273254706c109c
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-10-14 15:18:00 -0300

    Re-write process data and methods name

--- static/js/teacher.questions.js
+++ static/js/teacher.questions.js
@@ -1,74 +1,24 @@
 $(document).ready(function() {
-  /* Share/don't share question */
-  $("input[type='checkbox']").change(function(){ 
-    id_checkbox = $(this)[0].id;
-    share_question(id_checkbox);
-  });
-
   /* Preview question */
-  $("button[data-action='preview']").click(function() {
+  $("button[data-action='preview']").click(function() {  
     id_question = $(this)[0].id;
-    preview_question(id_question)
+    previewQuestion(id_question)
   });
 
   /* Download question */
   $("button[data-action='download']").click(function() {
     id_question = $(this)[0].id;
-    download_question(id_question)
+    downloadQuestion(id_question)
+  });
+
+  /* Share/don't share question */
+  $("input[type='checkbox']").change(function(){ 
+    id_checkbox = $(this)[0].id;
+    shareQuestion(id_checkbox);
   });
 });
 
-function share_question(id_checkbox) {
-  $.ajax({
-    type: 'POST',
-    url: document.location.pathname + "share/" + id_checkbox,
-    headers: {'X-CSRFToken': Cookies.get('csrftoken')},
-    dataType: 'json',
-  })
-  .done(function(data) {
-    if (data.code == "dontshare") {
-      $("#"+id_checkbox+".codigo").text("");
-    } else {
-      $("#"+id_checkbox+".codigo").text(data.code);
-    }
-  })
-  .fail(function() {
-    Materialize.toast('Un error a ocurrido. Intente nuevamente', 5000, 'rounded');
-  });
-};
-
-function preview_question(id_question) {
-  $.ajax({
-    type: "POST",
-    url: document.location.pathname,
-    headers: {'X-CSRFToken': Cookies.get('csrftoken')},
-    data: {
-      action: "preview",
-      id_question: id_question
-    },
-    dataType: 'json',
-  })
-  .done(function(data){
-    if (data.result == "error") {
-        Materialize.toast(data.message, 3000,'rounded');
-    } else {
-        //Materialize.toast('Pregunta valida', 3000,'rounded');
-        url           = data.url;
-        tipo_pregunta = data.type;
-        extension     = data.extension;
-        codigo        = data.code;
-        opcion        = "visualizar";
-        xml_archivo   = $.parseXML(data.archivo);
-        xml_manifest  = $.parseXML(data.imsmanifest);
-        select_method()
-    }
-  })
-  .fail(function() {
-    Materialize.toast('Un error a ocurrido. Intente nuevamente', 3000, 'rounded');
-  });
-};
-
-function download_question(id_question) {
+function downloadQuestion(id_question) {
   $.ajax({
     type: "POST",
     url: document.location.pathname+"download",
@@ -86,17 +36,7 @@ function download_question(id_question) {
           failCallback: function (html, url) {
             Materialize.toast('Error al obtener la pregunta', 3000, 'rounded');
           }
-        });
-        /*
-        console.log(jqXHR)
-        console.log(jqXHR.getAllResponseHeaders())
-        console.log(data)
-        console.log(data.length);
-        var buffer = new Uint8Array(str2bytes(data)).buffer;
-        var blob = new Blob([buffer], {type: content_type});
-        console.log(blob.size);
-        saveAs(blob, jqXHR.getResponseHeader("Name"));
-        */
+        }); 
       } else {
         Materialize.toast(data.message, 3000, 'rounded');
       }
@@ -110,28 +50,49 @@ function download_question(id_question) {
   //window.open(document.location.pathname+"/download/"+id_question, "_blank");
 };
 
-function str2bytes (str) {
-    /*
-    var bytes = new Uint8Array(str.length);
-    for (var i=0; i<str.length; i++) {
-        bytes[i] = str.charCodeAt(i);
+function previewQuestion(id_question) {  
+  $.ajax({
+    type: "POST",
+    url: document.location.pathname,
+    headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+    data: {
+      action: "preview",
+      id_question: id_question
+    },
+    dataType: 'json',
+  })
+  .done(function(data){
+    if (data.result == "error") {
+        Materialize.toast(data.message, 3000,'rounded');
+    } else {
+        //Materialize.toast('Pregunta valida', 3000,'rounded');        
+        setVariables(data);
+        selectMethod();
     }
-    */
-    var bytes = []; // char codes
-    var bytesv2 = []; // char codes
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    Materialize.toast('Un error a ocurrido. Intente nuevamente', 3000, 'rounded');
+    console.log(errorThrown);
+  });
+};
 
-    for (var i = 0; i < str.length; ++i) {
-      var code = str.charCodeAt(i);
 
-      bytes = bytes.concat([code]);
 
-      bytesv2 = bytesv2.concat([code & 0xff, code / 256 >>> 0]);
+function shareQuestion(id_checkbox) {
+  $.ajax({
+    type: 'POST',
+    url: document.location.pathname + "share/" + id_checkbox,
+    headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+    dataType: 'json',
+  })
+  .done(function(data) {
+    if (data.code == "dontshare") {
+      $("#"+id_checkbox+".codigo").text("");
+    } else {
+      $("#"+id_checkbox+".codigo").text(data.code);
     }
-
-    // 72, 101, 108, 108, 111, 31452
-    //console.log('bytes', bytes.join(', '));
-
-    // 72, 0, 101, 0, 108, 0, 108, 0, 111, 0, 220, 122
-    //console.log('bytesv2', bytesv2.join(', '));
-    return bytesv2;
-}
+  })
+  .fail(function() {
+    Materialize.toast('Un error a ocurrido. Intente nuevamente', 5000, 'rounded');
+  });
+};
