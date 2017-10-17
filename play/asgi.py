8cc2cae54177887c5bab67810329bfd507acb953
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-10-17 11:35:27 -0300

    Add description file

--- play/asgi.py
+++ play/asgi.py
@@ -1,4 +1,16 @@
+"""
+ASGI entrypoint file for default channel layer.
+
+Points to the channel layer configured as "default" so you can point
+ASGI applications at "play.asgi:channel_layer" as their channel layer.
+
+For more information on this file, see
+http://channels.readthedocs.io/en/stable/deploying.html?highlight=asgi.py#run-interface-servers
+https://github.com/andrewgodwin/channels-examples
+"""
+
 import os
+
 from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "play.settings")
