bf29f52d2c99d9e5952acf620a5d6359e6b3de60
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-10-25 12:18:48 -0300

    Modified routing

--- play/routing.py
+++ play/routing.py
@@ -2,7 +2,7 @@ from channels import route
 from apps.wsocket.consumers import *
 
 channel_routing = [
- # Las funciones se definen en consumers.py
- route("websocket.connect", ws_connect),
- route("websocket.receive", ws_receive),
+    route("websocket.connect", ws_connect, path=r"^/ws/play/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    #route("websocket.receive", ws_message, path=r"^/ws/play/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    route("websocket.disconnect", ws_disconnect, path=r"^/ws/play/(?P<room_name>[a-zA-Z0-9_]+)/$"),
 ]
