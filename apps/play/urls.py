063ccf2ae73e284d007bb7a855455d303b5da5bd
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-10-28 00:48:54 -0300

    Add url to list of closed plays

--- apps/play/urls.py
+++ apps/play/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from apps.play.views import *
 
 urlpatterns = [
+    url(r'^$', view_plays, name='index'),
     url(r'^stop$', stop_play, name='stop'), #Only POST
     url(r'^(?P<play_id_char>\w+)/$', show_play, name='show'),
     url(r'^(?P<play_id_char>\w+)/reply/(?P<question_id>\d+)/$', reply_play, name='reply'),
