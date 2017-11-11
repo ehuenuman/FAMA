6b8c1cdd8533ecb629e5f3e2c1efe49a21aa87cd
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-11-10 21:35:29 -0300

    Add function for execute procedure mysql

--- apps/play/models.py
+++ apps/play/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import connection, models
 
 # Create your models here.
 class Play(models.Model):
@@ -18,4 +18,20 @@ class Play(models.Model):
         ordering = ('-creation_play', '-start_play')
 
     def duration_to_time(self):
+        """Use in template for render duration time"""
         return "{0} min.".format(int(self.duration.seconds/60))
+
+    @staticmethod  
+    def total_for_question(id_play):
+        """Search for any question on play the total of corrects and incorrect"""        
+        cur = connection.cursor()
+        cur.callproc("total_for_question", [id_play,])
+        results = cur.fetchall()        
+        cur.close()
+        
+        total_for_question = []
+        for total in results:
+            total_for_question.append({"question": total[0], "replys": total[1], "corrects": total[2], "incorrects": total[3]})
+        
+        return total_for_question
+        #[Play(*row) for row in results]
