a1387fe6e8083ddcfa67a7fcac60cea3adef6310
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-11-13 15:38:15 -0300

    Add chart for summary answers

--- static/js/play.result.charts.js
+++ static/js/play.result.charts.js
@@ -1,4 +1,6 @@
 var played_formative_chart;
+var answer_formative_chart;
+
 function played_formative_chart(total_student, played_student) {
   played_formative_chart = c3.generate({
     bindto: "#students_play_chart",
@@ -7,9 +9,6 @@ function played_formative_chart(total_student, played_student) {
         ['Estudiantes', 0]
       ],
       type: 'gauge'
-    },
-    padding: {
-      bottom: 20
     },
     gauge: {
       label: {
@@ -30,6 +29,9 @@ function played_formative_chart(total_student, played_student) {
     size: {
       height: 120,
       width: 150
+    },
+    padding: {
+      bottom: 20
     }
   });
 
@@ -38,4 +40,50 @@ function played_formative_chart(total_student, played_student) {
         columns: [['Estudiantes', played_student]]
     });
   }, 500);   
-}
+};
+
+function answer_formative_chart(total_for_question) {
+  for (var i = 0; i < total_for_question.length; i++) {
+    total_for_question[i].question = "P"+(i+1);
+  }  
+
+  answer_formative_chart = c3.generate({
+    bindto: "#students_answer_chart",
+    data: {
+      json: total_for_question,
+      type: "bar",
+      groups: [
+        ["corrects", "incorrects"]
+      ],
+      keys: {
+        x: "question",
+        value: ["corrects", "incorrects"],
+      },
+      colors: {
+        corrects: "#4caf50",
+        incorrects: "#f44336"
+      },
+      names: {
+        corrects: "Correctas",
+        incorrects: "Incorrectas"
+      }
+    },
+    axis: {
+      x: {
+        type: "category"
+      },
+      y: {
+        label: {
+          text: "Cant. de estudiantes",
+          position: 'outer-middle'
+        }
+      }
+    },    
+    size: {
+      height: 240
+    },
+    padding: {      
+      right: 50
+    }
+  });
+};
