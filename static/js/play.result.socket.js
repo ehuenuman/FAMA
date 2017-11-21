6775e9cc0bb6d3b5610076ce3f5d41fb64e93f1b
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-11-21 13:28:44 -0300

    Adapt table result to new format of table result with row for result

--- static/js/play.result.socket.js
+++ static/js/play.result.socket.js
@@ -1,83 +1,93 @@
 socket.onmessage = function(e) {
   var data = JSON.parse(e.data);
   if (data.action == "answer") {
-    var cell = $("td[data-student='"+data.student+"'][data-question='"+data.question+"'] i");
+    cell = $("td[data-student='"+data.student+"'][data-question='"+data.question+"'] i");
+    console.log(cell);
 
-    if (cell.hasClass("reply correct")) {  //Era correcta
+    if (cell.hasClass("reply") && cell.hasClass("correct")) {  //Estaba correcta
+      console.log("Estaba correcta");
       if (data.correct == 0) { //Respondió incorrectamente
+        console.log("Respondió incorrectamente");
         cell
           .removeClass('fa-check correct')          
           .addClass("fa-times incorrect");
-        var student_total = parseInt(cell.parent().children().last().html());
+        var student_total = parseInt(cell.parent().parent().children().last().html());
         if (student_total > 0) {
-          cell.parent().children().last().html(student_total-1);
+          cell.parent().parent().children().last().html(student_total-1);
         }
-        var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects span").html());
+        var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects").html());        
         if (isNaN(questions_corrects)) {
-          $("td[data-question='"+data.question+"'] div.corrects span").html(0);
-          $("td[data-question='"+data.question+"'] div.incorrects span").html(1);
+          $("td[data-question='"+data.question+"'] div.corrects").html(0);
+          $("td[data-question='"+data.question+"'] div.incorrects").html(1);
         } else {
           if (questions_corrects > 0) {
-            var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects span").html());         
-            $("td[data-question='"+data.question+"'] div.incorrects span").html(questions_incorrects+1);
-            $("td[data-question='"+data.question+"'] div.corrects span").html(questions_corrects-1);
+            var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects").html());         
+            $("td[data-question='"+data.question+"'] div.incorrects").html(questions_incorrects+1);
+            $("td[data-question='"+data.question+"'] div.corrects").html(questions_corrects-1);
           }
         }
       }
     } else {
-      if (cell.hasClass("reply incorrect")) { // Estaba incorrecta
+      if (cell.hasClass("reply") && cell.hasClass("incorrect")) { // Estaba incorrecta
+        console.log("Estaba incorrecta");
         if (data.correct == 1) { // Respondio correcto
+          console.log("Respondió correcto");
           cell
             .removeClass('fa-times incorrect')            
             .addClass("fa-check correct");
-          var student_total = parseInt(cell.parent().children().last().html());
+          var student_total = parseInt(cell.parent().parent().children().last().html());
           if (student_total >= 0) {
-            cell.parent().children().last().html(student_total+1);
+            cell.parent().parent().children().last().html(student_total+1);
           }
-          var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects span").html());
+          var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects").html());
           if (isNaN(questions_corrects)) {
-            $("td[data-question='"+data.question+"'] div.corrects span").html(1);
-            $("td[data-question='"+data.question+"'] div.incorrects span").html(0);
+            $("td[data-question='"+data.question+"'] div.corrects").html(1);
+            $("td[data-question='"+data.question+"'] div.incorrects").html(0);
           } else {
             if (questions_corrects >= 0) {
-              var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects span").html());
-              $("td[data-question='"+data.question+"'] div.corrects span").html(questions_corrects+1);  
-              $("td[data-question='"+data.question+"'] div.incorrects span").html(questions_incorrects-1);            
+              var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects").html());
+              $("td[data-question='"+data.question+"'] div.corrects").html(questions_corrects+1);  
+              $("td[data-question='"+data.question+"'] div.incorrects").html(questions_incorrects-1);            
             }        
           }
         }
       } else { // No se había respondido
+        console.log("No había respondido");
         if (data.correct == 0) { // Respondio incorrectamente
+          console.log("Respondió incorrectamente");
           cell            
             .removeClass("fa-minus")
             .addClass("fa-times reply incorrect");
-          var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects span").html());
+          var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects").html());
           if (isNaN(questions_corrects)) {
-            $("td[data-question='"+data.question+"'] div.corrects span").html(0);
-            $("td[data-question='"+data.question+"'] div.incorrects span").html(1);
-          } else {
-            var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects span").html());         
-            $("td[data-question='"+data.question+"'] div.incorrects span").html(questions_incorrects+1);
-          }
+            $("td[data-question='"+data.question+"'] div.corrects").html(0);
+            $("td[data-question='"+data.question+"'] div.incorrects").html(1);
+          } else {          
+            var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects").html());
+            $("td[data-question='"+data.question+"'] div.incorrects").html(questions_incorrects+1);
+          }          
         } else { // Respondio correctamente
+          console.log("Respondió correctamente");
           cell
             .removeClass("fa-minus")
             .addClass("fa-check reply correct");            
-          var student_total = parseInt(cell.parent().children().last().html());
+          var student_total = parseInt(cell.parent().parent().children().last().html());
           if (student_total >= 0) {
-            cell.parent().children().last().html(student_total+1);
+            cell.parent().parent().children().last().html(student_total+1);
           }
-          var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects span").html());
+          var questions_corrects = parseInt($("td[data-question='"+data.question+"'] div.corrects").html());
           if (isNaN(questions_corrects)) {
-            $("td[data-question='"+data.question+"'] div.corrects span").html(1);
-            $("td[data-question='"+data.question+"'] div.incorrects span").html(0);
+            $("td[data-question='"+data.question+"'] div.corrects").html(1);
+            $("td[data-question='"+data.question+"'] div.incorrects").html(0);
           } else {
             if (questions_corrects >= 0) {
-              var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects span").html());
-              $("td[data-question='"+data.question+"'] div.corrects span").html(questions_corrects+1);              
+              var questions_incorrects = parseInt($("td[data-question='"+data.question+"'] div.incorrects").html());
+              $("td[data-question='"+data.question+"'] div.corrects").html(questions_corrects+1);              
             }
           }
         }
+        var question_total = parseInt($("td[data-question='"+data.question+"'] div.total").html());
+        $("td[data-question='"+data.question+"'] div.total").html(question_total+1);
       }
     }    
     answer_formative_chart.load({
