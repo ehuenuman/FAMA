6662ec5bb215c752064c4b47446b194b2bf96d15
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-11-21 13:47:38 -0300

    Add new procedure for when click in some alternative send answer to server

--- static/js/question.reply_choice.js
+++ static/js/question.reply_choice.js
@@ -27,4 +27,24 @@ $(document).ready(function() {
        Materialize.toast("Selecciona una alternativa", 1000, "rounded")
     }
   });
+
+  $("li.collection-item input").change(function(event) {
+    $.ajax({
+      url: document.location.pathname,
+      type: "POST",
+      headers: {"X-CSRFToken": Cookies.get("csrftoken")},
+      dataType: "JSON",
+      data: {answer: event.target.id},
+    })
+    .done(function(data) {
+      if (data.data == "OK") {
+        Materialize.toast("Respuesta guardada", 1000, "rounded")
+      } else {
+        Materialize.toast(data.data, 1000, "rounded")
+      }
+    })
+    .fail(function() {
+      Materialize.toast("Un error ha ocurrido", 1000, "rounded")
+    });
+  });
 });
