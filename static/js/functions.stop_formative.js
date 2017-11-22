07382e7d66e5b40afe0eae1e56908e9508d3e28f
Esteban Huenuman <esteban.huenuman@hotmail.cl>
2017-11-22 11:38:28 -0300

    Rename file. Now show modal with confirm button after the stop formative

---
+++ static/js/functions.stop_formative.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  
+  var playId;
+  var url;
+  $(".btn.stop-formative").click(function(event) {
+    playId = $(event.currentTarget).data("play-id");
+    url = $(event.currentTarget).data("url");
+    $(".modal.stop-formative").modal("open", {
+      dismissible: false
+    });
+  });
+
+  $(".modal.stop-formative a.stop-formative").click(function(event) {    
+    $.ajax({
+      url: url,
+      type: "POST",
+      headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+      dataType: "json",      
+      data: { playId: playId },
+    })
+    .done(function(data) {      
+      if (data.data == "OK") {
+        document.location.href = document.location.pathname;
+      } 
+    })
+    .fail(function(jqXHR, textStatus, e) {
+      console.log("Error:", e);
+    });
+  });
+
+});
