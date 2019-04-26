503b7862864f32099e0a676e010ed5f5dc405f48
Juan Latorre <juanlatorre@protonmail.com>
2019-04-25 20:33:51 -0400

    arreglo de posicion deslizador

--- static/js/library/materialize.js
+++ static/js/library/materialize.js
@@ -3192,7 +3192,7 @@ if (jQuery) {
     var showRangeBubble = function(thumb) {
       var paddingLeft = parseInt(thumb.parent().css('padding-left'));
       var marginLeft = (-7 + paddingLeft) + 'px';
-      thumb.velocity({ height: "30px", width: "30px", top: "-30px", marginLeft: marginLeft}, { duration: 300, easing: 'easeOutExpo' });
+      thumb.velocity({ height: "30px", width: "30px", top: "-25px", marginLeft: marginLeft}, { duration: 300, easing: 'easeOutExpo' });
     };
 
     var calcRangeOffset = function(range) {
