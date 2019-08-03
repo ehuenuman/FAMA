3ab200f1fac9f00ea86853e3f10124df6ca0fbf2
Juan Latorre <juanlatorre@protonmail.com>
2019-08-03 16:20:21 -0400

    .

--- play/settings.py
+++ play/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
 # Authentication backends
 # https://docs.djangoproject.com/en/1.10/topics/auth/customizing/
 
-AUTHENTICATION_BACKENDS = ( 
+AUTHENTICATION_BACKENDS = (
     #'play.login.LoginBackend',
     'django.contrib.auth.backends.ModelBackend',
     )
@@ -159,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/opt/fama/play/static'
+STATIC_ROOT = '/home/juanlatorre/Repos/fama/static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
